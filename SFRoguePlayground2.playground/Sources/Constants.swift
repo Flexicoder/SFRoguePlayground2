7b6efbbaa9a8512802e0418e5d769efb580ccb56
Paul Ledger <1398301+Flexicoder@users.noreply.github.com>
2020-04-29 12:34:13 +0100

    Make tilesize an even number

--- SFRoguePlayground2.playground/Sources/Constants.swift
+++ SFRoguePlayground2.playground/Sources/Constants.swift
@@ -1,6 +1,6 @@
 enum Constants {
     struct Constraints {
-        static let tileSize = 5                 // The rendered tile size
+        static let tileSize = 4                 // The rendered tile size
         static let minimumExtent = 3            // The minimum number of rows or columns
         static let maximumExtent = 9            // The maximum number of rows or columns
         static let minimumPositionOffset = 10   // Minimum random offset for positioning a room
